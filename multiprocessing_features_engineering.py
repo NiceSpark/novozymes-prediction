dc2cf86d29eb1560227f5a7f48c0043b5870e81b
Thomas <t.goyet@protonmail.com>
2022-11-24 09:33:33 +0100

    no more fragmentation + rosetta scores

--- multiprocessing_features_engineering.py
+++ multiprocessing_features_engineering.py
@@ -13,23 +13,24 @@ from utils.features_biopython import add_structure_infos, add_protein_analysis, 
 from utils.demask_features import add_demask_predictions
 from utils.file_utils import open_json
 from utils.infos_translation import aa_char2int
+from utils.rosetta_features import add_rosetta_scores
 
-THREADS = 12
+THREADS = 10
 
-ADD_STRUCTURE_INFOS = True
+ADD_STRUCTURE_INFOS = False  # this is done on GCP with 32 threads...
 ADD_DEMASK_PREDICTIONS = True
+ADD_ROSETTA_SCORES = True
 ADD_PROTEIN_ANALYSIS = True
 ONLY_DDG = True
 SAVE_NEW_CSV = True
 CONVERT_MUTATION_TO_INT = True
 CLEAN_DF = True
 START_FRESH = True
-DROP_COLUMNS = False
 
 
 NAME = "all_v3"
 DATASET_DIR = f"./data/main_dataset_creation/outputs/{NAME}/"
-DATASET_INPUT_PATH = DATASET_DIR+"dataset_with_esm_features.csv"
+DATASET_INPUT_PATH = DATASET_DIR+"dataset_with_structure_features.csv"
 DATASET_OUTPUT_PATH = DATASET_DIR+"dataset_with_all_features.csv"
 
 
@@ -52,8 +53,9 @@ if CLEAN_DF:
     # print(df.isna().sum().to_dict())
 
 # ADD all columns that will be computed
+new_columns = []
 if ADD_DEMASK_PREDICTIONS:
-    new_columns = [
+    new_columns += [
         "direct_demask_entropy",
         "direct_demask_log2f_var",
         "direct_demask_matrix",
@@ -63,30 +65,39 @@ if ADD_DEMASK_PREDICTIONS:
         "indirect_demask_matrix",
         "indirect_demask_score"
     ]
-    for column in new_columns:
-        df[column] = 0.0
 
 if ADD_STRUCTURE_INFOS:
-    new_columns = []
     for prefix in ["alphafold", "wild_relaxed", "mutated_relaxed", "mutation"]:
         new_columns += [f"{prefix}_{k}" for k in DSSP_Data_Keys[2:]]
         new_columns += [f"{prefix}_{k}" for k in ["sasa", "residue_depth",
                                                   "c_alpha_depth", "bfactor"]]
-    for col in new_columns:
-        df[col] = np.nan
+
+if ADD_ROSETTA_SCORES:
+    columns = ["dslf_fa13", "fa_atr", "fa_dun", "fa_elec", "fa_intra_rep", "fa_intra_sol_xover4", "fa_rep", "fa_sol",
+               "hbond_bb_sc", "hbond_lr_bb", "hbond_sc", "hbond_sr_bb", "linear_chainbreak", "lk_ball_wtd", "omega",
+               "overlap_chainbreak", "p_aa_pp", "pro_close", "rama_prepro", "ref", "total_score", "yhh_planarity"]
+    prefixes = ["alphafold", "wild_relaxed", "mutated_relaxed", "mutation"]
+
+    for prefix in prefixes:
+        for col in columns:
+            new_columns.append(f"{prefix}_{col}")
 
 if ADD_PROTEIN_ANALYSIS:
-    new_columns = ["charge_at_pH", "mutation_molecular_weight", "mutation_aromaticity", "mutation_isoelectric_point", "mutation_helix_fraction",
-                   "mutation_turn_fraction", "mutation_sheet_fraction", "mutation_molar_extinction_1", "mutation_molar_extinction_2", "mutation_gravy",
-                   "blosum62", "blosum80", "blosum90"]
-    for col in new_columns:
-        df[col] = np.nan
+    new_columns += ["charge_at_pH", "flexibility", "gravy", "molar_extinction_2", "molar_extinction_1", "sheet_fraction",
+                    "turn_fraction", "helix_fraction", "isoelectric_point", "aromaticity", "instability_index", "molecular_weight"]
+    new_columns += ["mutation_molecular_weight", "mutation_aromaticity", "mutation_isoelectric_point", "mutation_helix_fraction",
+                    "mutation_turn_fraction", "mutation_sheet_fraction", "mutation_molar_extinction_1", "mutation_molar_extinction_2", "mutation_gravy"]
+    new_columns += ["blosum62", "blosum80", "blosum90", "blosum100"]
 
+new_columns_df = pd.DataFrame(columns=new_columns)
+df = pd.concat([df, new_columns_df], axis=1)
 # Split df by uniprot
 unique_uniprot = df.uniprot.unique()
 all_uniprot_dfs = [df[df.uniprot.eq(uniprot)].copy()
                    for uniprot in unique_uniprot]
-print(all_uniprot_dfs[0].isna().sum().to_dict())
+print({k: v for k, v in
+       all_uniprot_dfs[0].isna().sum().to_dict().items()
+       if v > 0})
 
 
 def compute_features(df):
@@ -100,6 +111,9 @@ def compute_features(df):
         df = add_structure_infos(df, compute_sasa=True,
                                  compute_depth=True, compute_dssp=True, compute_bfactor=True,
                                  multiprocessing=True)
+
+    if ADD_ROSETTA_SCORES:
+        df = add_rosetta_scores(df, multiprocessing=True)
 
     if ADD_PROTEIN_ANALYSIS:
         df = add_protein_analysis(df, multiprocessing=True)
@@ -122,24 +136,8 @@ print(result_dfs[0].isna().sum().to_dict())
 main_df = pd.concat(result_dfs)
 
 print(len(main_df))
-print(main_df.isna().sum().to_dict())
+print({k: v for k, v in main_df.isna().sum().to_dict().items() if v > 0})
+print(main_df.head())
 
 if SAVE_NEW_CSV:
-    if DROP_COLUMNS:
-        df.drop(columns=["mutation_code", "AlphaFoldDB"], inplace=True)
-    # if ONLY_DDG:
-    #     df.drop(columns=["dTm", "Tm"], inplace=True)
-
-    ordered_columns = open_json("./data/features.json")
-    ordered_columns = sum([ordered_columns[k] for k in ordered_columns], [])
-
-    # for col in ordered_columns:
-    #     if col not in df.columns.to_list():
-    #         df[col] = ""
-
-    # df = df[ordered_columns]
-
     main_df.to_csv(DATASET_OUTPUT_PATH, index=False)
-    features_columns = ordered_columns[4:-4]
-    print(main_df.isna().sum())
-    print(main_df.head())
