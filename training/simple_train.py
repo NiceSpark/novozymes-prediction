cbb9552bbabd38af543ba2d7d2b1235dd59579e1
Thomas <t.goyet@protonmail.com>
2022-12-20 19:09:02 +0100

    script with argparse

--- training/simple_train.py
+++ training/simple_train.py
@@ -1,44 +1,63 @@
-from training_utils.file_utils import open_json, log_kfold_training
+#! /usr/bin/python3
+import argparse
+
+from training_utils.file_utils import open_json
+from training_utils.save_results import log_kfold_training, move_models_and_scalers
 from training_utils.models import HybridNN
 from training_utils.model_utils import *
-from training_utils.training import k_fold_training
+from training_utils.training_nn import k_fold_training_nn
+from training_utils.training_xgboost import k_fold_training_xgboost
 
-KEEP_MODELS = True
+# use argparse to get config: xgboost or hybrid, keep_models
+parser = argparse.ArgumentParser()
+# add argument xgboost if you want to use xgboost
+parser.add_argument("--xgboost", action="store_true")
+parser.add_argument("--keep_models", action="store_true")
+args = parser.parse_args()
 
-config = open_json("hybrid_nn_config.json")
+if args.xgboost:
+    config = open_json("config_xgboost.json")
+else:
+    config = open_json("config_hybrid_nn.json")
+    device = get_device(config)
+    # if there is only one target replace "dTm_loss_coef" with either 1 (dTm in targets) or 0 (ddG in targets)
+    if config["targets"] == ["ddG"]:
+        config["dTm_loss_coef"] = 0
+    elif config["targets"] == ["dTm"]:
+        config["dTm_loss_coef"] = 1
+
 features_dict = open_json(
     f"{config['dataset_dir']}/{config['features_name']}.json")
 features, features_infos = compute_feature_list(config, features_dict)
-device = get_device(config)
 log_name = config["model_type"]
 
-print(len(features))
-print(len(features_infos["direct_features"]))
-print(config["batch_size"])
+print(f"using {len(features_infos['direct_features'])} direct features")
 
 df = load_dataset(config, features, rm_nan=True)
-
-all_training_results = {"simple_train": [],
-                        "total_training_time": 0}
-
-# add protein_index to the dataset and get ksplit:
+# add kfold group to the dataset and get ksplit:
 df = split_dataset(df, config)
-# training
-training_results = k_fold_training(
-    df, config, features, features_infos, device, keep_models=KEEP_MODELS)
 
-# add training results to all the other ones
-all_training_results["simple_train"] = training_results
+# training
+if args.xgboost:
+    print("training with xgboost")
+    training_results = k_fold_training_xgboost(
+        df, config, features, features_infos, keep_models=args.keep_models)
+    model_structure = []
+else:
+    print("training with hybrid nn")
+    training_results = k_fold_training_nn(
+        df, config, features, features_infos, device, keep_models=args.keep_models)
+    # save results to output
+    model = HybridNN(config)
+    model_structure = str(model).replace(
+        '(', '').replace(')', '').split('\n')
 
 # save results to output
-model = HybridNN(
-    len(features_infos["direct_features"]), config)
-model_structure = str(model).replace(
-    '(', '').replace(')', '').split('\n')
 dir_path = log_kfold_training(
-    log_name, all_training_results, config, features, model_structure)
+    log_name, training_results, config, features, model_structure)
 
-if KEEP_MODELS:
+# move models and scalers to output
+if args.keep_models:
     move_models_and_scalers(dir_path)
 
 print(f"logged training in {dir_path}")
